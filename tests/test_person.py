3bcc5961d9c4cf96e9ca5bfad625fe0cdf571746
Austin Roy <austin.roy@andela.com>
2016-11-09 12:26:43 +0300

    Added docstrings for test_person.py

--- tests/test_person.py
+++ tests/test_person.py
@@ -10,16 +10,20 @@ class TestPerson(unittest.Testcase):
 		self.person = Person ()
 
 	def test_add_person (self):
+		""" Tests the add person function"""
 		pass
 
 	def test_rellocate_person(self):
+		"""Tests rellocation function"""
 		pass
 
 	def test_fellow_subclass_of_person(self):
+		"""Tests if class Fellow is a subclass of class Person"""
 		self.asserTrue((issubclass(Fellow,Person)),
 						 msg='Class Fellow should be a subclass of Person')
 
 	def test_staff_subclass_of_person(self):
+		"""Tests if class Staff is a subclass of class Person"""
 		self.asserTrue((issubclass(Staff,Person)),
 						 msg='Class Staff should be a subclass of Person')
 
