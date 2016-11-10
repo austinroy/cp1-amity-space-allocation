ae76557e13cc28fa9f1e475819d0962f56f2cb78
austin.roy@andela.com <austinroy@brian-ThinkPad-E460localhost.org>
2016-11-10 11:23:29 +0300

    Fixes on test_amity.py

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -6,6 +6,9 @@ from amity import Amity
 class TestRoom(unittest.TestCase):
     """Unit tests to test functions in the Person Class"""
 
+    def setUp(self):
+        amity = Amity()
+
 
 if __name__ == '__main__':
     unittest.main()
