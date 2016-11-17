e1e5aa6632863b219ce20ce694ec9bd6c35b22f0
Austin Roy <austinroy@andela.com>
2016-11-17 19:04:40 +0300

    Changed system structure

--- amity.py
+++ amity.py
@@ -3,3 +3,30 @@ class Amity(object):
 
     def __init__(self):
         pass
+
+    def add_person():
+        pass
+
+    def reallocate_person():
+        pass
+
+    def create_room():
+        pass
+
+    def load_people():
+        pass
+
+    def print_room():
+        pass
+
+    def load_state():
+        pass
+
+    def save_state():
+        pass
+
+    def print_allocations():
+        pass
+
+    def print_unallocated():
+        pass
--- tests/test_amity.py
+++ tests/test_amity.py
@@ -2,6 +2,8 @@ import unittest
 
 from amity import Amity
 
+from room import Office, LivingSpace
+
 
 class TestRoom(unittest.TestCase):
     """Unit tests to test functions in the Person Class"""
@@ -9,16 +11,48 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.amity = Amity()
 
-    def test_print_allocations(self):
-        """Tests that allocations are printed"""
-        pass
+        self.amity.create_room({
+            "<room_name>": ["LivingSpace1"],
+            "Living": True,
+            "Office": False
+        })
+        """Create sample living space"""
+        self.amity.create_room({
+            "<room_name>": ["Office1"],
+            "Living": False,
+            "Office": True
+        })
+        self.office1 = self.amity.offices[0]
 
-    def test_print_unallocated(self):
-        """Tests that unallocated people are printed"""
-        pass
+    def test_create_room(self):
+        """Test creation of rooms"""
+        # Check rooms created added to relevant lists
+        self.assertEqual(2, len(self.room.rooms))
+        self.assertEqual(1, len(self.room.livingspaces))
+        self.assertEqual(1, len(self.room.offices))
 
-    def test_load_people(self):
-        """Tests loading of people from text file"""
+        """Validates that duplicate rooms are not added"""
+        self.room.create_room({
+            "<room_name>": ["Office1"],
+            "Living": False,
+            "Office": True
+        })
+        # Duplicate rooms not added to any list
+        self.assertEqual(2, len(self.room.rooms))
+        self.assertEqual(1, len(self.room.offices))
+
+    def test_office_has_capacity_of_6(self):
+        """Ensure office has a capacity of 6"""
+        office = Office()
+        self.assertEqual(6, office.max_occupancy)
+
+    def test_livingspace_has_capacity_of_4(self):
+        livingspace = LivingSpace()
+        self.assertEqual(4, livingspace.max_occupancy)
+        """Ensure livingspace has a capacity of 4"""
+
+    def test_print_room():
+        """Tests for the print_room function"""
         pass
 
 
